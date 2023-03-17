d1e392138e6c32fc5ae4ba2cafcf9bd57c5954c5
!!Programmer!! <yonghayden68@gmail.com>
2023-03-17 15:37:14 +0800

    feat: errorlogger & export

---
+++ src/Functions/ErrorLogger.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Error | undefined} error
+ * @returns {void}
+ */
+function ErrorLogger(error) {
+    const XHR = new XMLHttpRequest();
+
+    XHR.open("POST", "/log");
+
+    XHR.setRequestHeader("Content-Type", "application/json");
+
+    XHR.onload = function () {
+        if (this.status === 200) {
+            console.log(`Error logged successfully.`);
+        } else {
+            console.log(`Error logging failed.`);
+        }
+    };
+
+    const data = { error: error.message };
+    XHR.send(JSON.stringify(data));
+}
