a9bf086222f6c357aa1219ca06836992757deb5d
!!Programmer!! <yonghayden68@gmail.com>
2023-03-18 14:10:20 +0800

    style: embed

--- src/Commands/Public/ping.js
+++ src/Commands/Public/ping.js
@@ -1,13 +1,66 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+    SlashCommandBuilder,
+    CommandInteraction,
+    EmbedBuilder,
+    italic,
+} = require("discord.js");
+const { ExtendedClient } = require("../../Base/index.js");
+const moment = require("moment");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with the latency."),
-    async execute(interaction) {
-        await interaction.reply({
-            content: `Pong! Latency is ${interaction.client.ws.ping}ms.`,
-            ephemeral: true,
-        });
+    /**
+     * @param {CommandInteraction} interaction
+     * @param {ExtendedClient} client
+     */
+    async execute(interaction, client) {
+        const websocketPing = client.ws.ping;
+        const now = moment.now();
+
+        const time = moment.duration(2, "seconds").asMilliseconds();
+
+        const pingEmbed = new EmbedBuilder()
+            .setTitle(`${client.config.emojis.check} Pong!`)
+            .addFields([
+                {
+                    name: "WebSocket Ping",
+                    value: websocketPing + "ms",
+                    inline: true,
+                },
+                {
+                    name: `Interaction Ping`,
+                    value: `${client.config.emojis.loading}`,
+                    inline: true,
+                },
+            ])
+            .setColor(client.config.embeds.colours.transparent);
+
+        interaction
+            .reply({
+                embeds: [pingEmbed],
+            })
+            .then((value) => {
+                const editedNow = moment.now();
+                const editedEmbed = pingEmbed.setFields([
+                    {
+                        name: "WebSocket Ping",
+                        value: websocketPing + "ms",
+                        inline: true,
+                    },
+                    {
+                        name: `Interaction Ping`,
+                        value: `${editedNow - now}ms`,
+                        inline: true,
+                    },
+                ]);
+
+                setTimeout(() => {
+                    value.edit({
+                        embeds: [editedEmbed],
+                    });
+                }, time);
+            });
     },
 };
