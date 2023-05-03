8ba72f6c6e6f22194e642ef38db8b20ae0142f5b
!!Programmer!! <yonghayden68@gmail.com>
2023-05-03 23:17:34 +0800

    fix: removed logging part, idek what to do with it

---
+++ src/Commands/Test/permissions.js
@@ -0,0 +1,37 @@
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    codeBlock,
+} = require("discord.js");
+const { ExtendedClient } = require("../../Base/index.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("permissions")
+        .setDescription("Lists out a user's permission.")
+        .addUserOption((option) => {
+            return option
+                .setName(`user`)
+                .setDescription(`The user you want to get their permissions.`)
+                .setRequired(false);
+        }),
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {ExtendedClient} client
+     */
+    async execute(interaction, client) {
+        const { options, guild } = interaction;
+        const user = options.getUser(`user`) || interaction.user;
+
+        const member = guild.members.cache.get(user.id);
+
+        if (user) {
+            const permissions = member.permissions;
+
+            interaction.reply({
+                content: `${codeBlock(permissions.toArray().join(", "))}`,
+                ephemeral: true,
+            });
+        }
+    },
+};
