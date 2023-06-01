99a348a6c6c9dda3c78d44cb8d434b8db43c19bd
!!Programmer!! <yonghayden68@gmail.com>
2023-06-01 17:30:54 +0800

    feat: new templates (button, select-menu)

---
+++ src/Templates/button.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder, CommandInteraction } = require("discord.js");
+const { ExtendedClient } = require("../../Base/index.js");
+
+module.exports = {
+    id: "",
+    /**
+     * @param {CommandInteraction} interaction
+     * @param {ExtendedClient} client
+     */
+    async execute(interaction, client) {},
+};
